d34055d7c8824b82f84c09c2fcb62cc1c6f3400b
Joshua Smith <joshua.smith@campbellsci.com>
2021-12-19 15:23:39 -0700

    add graphics skeleton

---
+++ day_01/scripts/graphics.js
@@ -0,0 +1,12 @@
+GameRoot.graphics = (function () {
+
+	let canvas = document.getElementById('canvas-main');
+	let context = canvas.getContext('2d');
+
+    function drawRectangle() {}
+
+    return {
+        drawRectangle,
+    };
+})();
+
